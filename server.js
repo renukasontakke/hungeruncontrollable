d914ef7b27c06c7237f49419ae2f22848f0f5e57
renuka sontakke <renuka.b.sontakke@gmail.com>
2019-11-13 15:05:42 +0530

    modifiedserver

--- server.js
+++ server.js
@@ -1,21 +1,6 @@
-var express = require("express");
-//var emproutes = require("./emp");
-//var adminroutes = require("./admin");
-var app = new express();
+const express = require('express')
+const app = express()
+const port = 3000
 
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
-app.use(express.json());
-//app.use("/employees",emproutes);
-//app.use("/admins",adminroutes);
-
-
-//app.listen(9000,function(){
-  //  console.log("server started");
-//})
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port port!`))
