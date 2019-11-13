daeda166e640578379a664cfeb6beccc092a67f6
renuka sontakke <renuka.b.sontakke@gmail.com>
2019-11-13 15:33:45 +0530

    modified server with put

--- server.js
+++ server.js
@@ -4,4 +4,5 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.post('/', (req, res) => res.send('Hello World!'))
+app.put('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port port!`))
